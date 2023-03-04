54deb9c9e7f37d3c24434ac8280c392a4c7f9822
Travis Perdue <travis.perdue@rallyhealth.com>
2023-03-04 16:09:10 -0600

    solve part1

--- day6/src/main/scala/example/fileio/PacketReader.scala
+++ day6/src/main/scala/example/fileio/PacketReader.scala
@@ -17,38 +17,32 @@ class PacketReaderImpl extends PacketReader {
   // Read the next packet into the packet start marker.
   // If have enough packets to be a valid marker, drop the oldest packet and read in new one.
   // Otherwise just read in the new packet.
-  def read(marker: PacketStartMarker, packet: Packet): PacketStartMarker = {
-    if (marker.packets.size == MARKER_SIZE) {
-      // check dup and create packet marker appropriately
-      PacketStartMarker(packet +: marker.packets.take(3), marker.position + 1, None)
-    } else {
-      PacketStartMarker(packet +: marker.packets, marker.position + 1, None)
+  // create packet marker and check dup appropriately
+  def read(marker: PacketStartMarker, packet: Packet): PacketStartMarker =
+    PacketStartMarker(packet +: marker.packets.take(3), marker.position + 1, marker.dupLocation).pipe(processFullMarker)
+
+  def isStart(marker: PacketStartMarker): Boolean = marker.dupLocation.isEmpty && marker.packets.size == MARKER_SIZE
+
+  def processFullMarker(marker: PacketStartMarker): PacketStartMarker =
+    marker.tail.find(p => p === marker.head) match {
+      case Some(dup) => markDup(marker, Some(marker.head.position, dup.position))
+      case None => checkDupInTail(marker)
     }
-  }
 
-  def isStart(marker: PacketStartMarker): Boolean = {
-    !marker.packets.tail.contains(marker.packets.head) && marker.dupLocation.isEmpty && marker.packets.size == MARKER_SIZE
-  }
-
-  def processFullMarker(marker: PacketStartMarker, packet: Packet): PacketStartMarker = {
-    marker.packets.find(p => p === packet) match {
-      case Some(dup) => markDup(marker, Some(packet.position, dup.position))
-      case None => markDup(marker, None)
+  def markDup(marker: PacketStartMarker, dup: Option[(Int, Int)]): PacketStartMarker =
+    dup match {
+      case Some(_) => marker.copy(dupLocation = List(marker.dupLocation, dup).flatten.maxByOption(_._2))
+      case _ => marker.copy(dupLocation = None)
     }
-  }
 
-  def markDup(marker: PacketStartMarker, dup: Option[(Int, Int)]): PacketStartMarker = {
-    marker.copy(dupLocation = dup)
-  }
-  def processNextPacket(marker: PacketStartMarker, packet: Packet): PacketStartMarker = {
+  def checkDupInTail(marker: PacketStartMarker): PacketStartMarker =
     marker.dupLocation.map { dupPosition =>
       dupPosition.bimap[Boolean, Boolean](
-        x => marker.packets.exists(_.position == x),
-        y => marker.packets.exists(_.position == y),
+        x => marker.tail.exists(_.position == x),
+        y => marker.tail.exists(_.position == y),
       )
     } match {
       case Some((true, true)) => marker
       case _ => markDup(marker, None)
     }
-  }
 }
--- day6/src/main/scala/example/models/PacketStartMarker.scala
+++ day6/src/main/scala/example/models/PacketStartMarker.scala
@@ -1,8 +1,7 @@
 package example.models
 
 case class PacketStartMarker(packets: Vector[Packet], position: Int, dupLocation: Option[(Int, Int)]) {
+  def head: Packet = packets.head
+  def tail: Vector[Packet] = packets.tail
 }
 
-object PacketStartMarker {
-//  def apply(packets: Vector[Packet]):
-}
